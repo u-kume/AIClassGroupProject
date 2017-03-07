48a096b5a39f0c782a7ac17402c9e52030b67661
John P. Baggs <johnpbaggs@gmail.com>
2017-03-06 23:33:58 -0600

    Created UnityProject Folder and Put game project inside

---
+++ UnityProject/AngryBots2.0/Assets/Scripts/AIAgent/AIAgentMovement.js
@@ -0,0 +1,55 @@
+﻿#pragma strict
+
+public var horizontalMovement : float = 0;
+public var verticalMovement : float = 0;
+
+public var player : Transform;
+
+private var playerLastPosition : Vector3;
+private var stuckCheckPosition : Vector3;
+private var timeSinceLastCheck : float;
+private var leftOrRight : float; // 1 = Right and -1 = Left
+private var upOrDown : float; // 1 = Up and -1 = Down
+
+
+
+function Awake () {
+	playerLastPosition = player.position;
+	timeSinceLastCheck = Time.time;
+	leftOrRight = 1;
+	upOrDown = -1;
+	stuckCheckPosition = player.position;
+}
+
+function Update () {
+//	Debug.Log(Time.realtimeSinceStartup + " playerLastPos = " + playerLastPosition + "   player pos = " + player.position);
+
+	move();
+
+}
+
+function move() {
+	if(playerLastPosition == player.position) {
+		changeVariables (0, upOrDown); //go down
+	}
+	else {
+		
+		changeVariables(leftOrRight, 0); //go right
+	}
+	if(Time.time - timeSinceLastCheck > .1)
+		checkPosition();
+	playerLastPosition = player.position;
+}
+
+function checkPosition () {
+	if(Vector3.Distance(player.position, stuckCheckPosition) < .05)
+		leftOrRight *= -1;
+	stuckCheckPosition = player.position;
+	timeSinceLastCheck = Time.time;
+}
+
+
+function changeVariables (hor : int, vert : int) {
+	horizontalMovement = hor;
+	verticalMovement = vert;
+}
---
+++ UnityProject/AngryBots2.0/Assets/Scripts/AIAgent/AIAgentTryNav.js
@@ -0,0 +1,62 @@
+﻿#pragma strict
+
+
+public var target : Transform;
+public var targetScript : AiAgentTargetList;
+
+public var timeSinceStop : float;
+private var timeSinceStuck : float;
+private var timeSinceChanged : float;
+private var changed : boolean;
+public var stopped : boolean;
+
+private var playerLastPosition : Vector3;
+private var stuckPosition : Vector3;
+
+
+var navmeshAgent : NavMeshAgent;
+
+function Start () {
+	playerLastPosition = transform.position;
+	//navmeshAgent = GetComponent.<NavMeshAgent>();
+	navmeshAgent.updateRotation = false;
+	//var targetScript : AiAgentTargetList = transform.GetComponent(AiAgentTargetList);
+	targetScript.findClosestTarget();
+	stopped = false;
+	//transform.Rotate(m)
+}
+
+function Update () {
+	targetScript.findClosestTarget();
+	
+	if(stopped == true){
+		//Debug.Log (Time.time - timeSinceStop + " hererere");
+		if(Time.time - timeSinceStop > 3) {
+			Debug.Log("making new selection from stop");
+			targetScript.setCurrent();
+			navmeshAgent.Resume();
+			setNewDestination();
+			stopped = false;
+		}
+	} else
+		if(Vector3.Distance(transform.position, targetScript.closestTarget) < 5 && stopped == false) {
+			Debug.Log("Stopping");
+			navmeshAgent.Stop(true);
+			stopped = true;
+			timeSinceStop = Time.time;
+		} else {
+			Debug.Log ("in Else");
+			setNewDestination();
+			}
+			
+	playerLastPosition = transform.position;
+}
+
+function setNewDestination() {
+	if(targetScript.closestTargetIndex != -1)
+		navmeshAgent.SetDestination(targetScript.closestTarget);
+	else 
+		navmeshAgent.SetDestination(transform.position);
+}
+
+
