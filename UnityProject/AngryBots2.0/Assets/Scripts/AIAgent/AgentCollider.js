e464734a3b23bb4a6241a886938ac314164d3062
John P. Baggs <johnpbaggs@gmail.com>
2017-03-10 17:51:07 -0600

    Added a Function for Stoping navigation mesh
    The navigation mesh now stops whenever an enemy is inside the collider
    and the player agent can see the enemy

--- UnityProject/AngryBots2.0/Assets/Scripts/AIAgent/AgentCollider.js
+++ UnityProject/AngryBots2.0/Assets/Scripts/AIAgent/AgentCollider.js
@@ -1,15 +1,21 @@
 ﻿#pragma strict
 
-public var enemyTriggerCollider : SphereCollider;
+public var enemyTriggerCollider : Collider;
 
 
-private var enemyTriggered : GameObject;
+private var enemyTriggered : GameObject = null;
 
 private var navigationAgentEnables : boolean = true;
 
+private var enemyTriggerColliderList : System.Collections.Generic.List.<GameObject> = new System.Collections.Generic.List.<GameObject> ();
+private var listSize : int = 0;
+
+
 function OnTriggerEnter (other : Collider) {
-	if(other.gameObject.tag == "Enemy") {
-			enemyTriggered = other.gameObject;
+	if(other.gameObject.tag == "Enemy" && canSeeEnemy(other) == true) {
+			enemyTriggerColliderList.Add(other.gameObject);
+			listSize++;
+			//enemyTriggered = other.gameObject;
 			Debug.Log("Enemy has entered trigger " + other.gameObject.name + "   " + other.gameObject.GetInstanceID());
 			gameObject.GetComponent.<AIAgentTryNav>().enabled = false;
 			navigationAgentEnables = false;
@@ -17,9 +23,12 @@ function OnTriggerEnter (other : Collider) {
 }
 
 function OnTriggerExit (other : Collider) {
-	if(other.gameObject.tag == "Enemy") {
+	if(other.gameObject.tag == "Enemy" && enemyTriggerColliderList.IndexOf(other.gameObject) != -1) {
 			Debug.Log("Enemy has exited trigger");
-			if(navigationAgentEnables == false) {
+			//enemyTriggerColliderList.IndexOf(other.gameObject);
+			enemyTriggerColliderList.Remove(other.gameObject);
+			listSize--;
+			if(navigationAgentEnables == false && listSize == 0) {
 				gameObject.GetComponent.<AIAgentTryNav>().enabled = true;
 				navigationAgentEnables = true;
 			}
@@ -27,11 +36,31 @@ function OnTriggerExit (other : Collider) {
 }
 
 function Update() {
-	Debug.Log("enemyTriggered = " + enemyTriggered.name + "   " + enemyTriggered.GetInstanceID());
-	if(navigationAgentEnables == false) {
-		if(enemyTriggered == null) {
+	//var numberOfChanged : int = 0;
+	//if(enemyTriggered != null) 
+		//Debug.Log(Time.realtimeSinceStartup + "listSize = " + listSize);
+	//if(navigationAgentEnables == false) {
+		if(listSize == 0) {
 			navigationAgentEnables = true;
 			gameObject.GetComponent.<AIAgentTryNav>().enabled = true;
+		} else {
+			for(var count : int = 0; count < listSize; count++) {
+				if(enemyTriggerColliderList.Item[count] == null) {
+					enemyTriggerColliderList.RemoveAt(count);
+					listSize--;
+					count--;
+				}
+			}
 		}
+	//}
+}
+
+public function canSeeEnemy(enemyToCheck : Collider) {
+	var enemyDirection : Vector3 = (enemyToCheck.transform.position - transform.position);
+	var hit : RaycastHit;
+	Physics.Raycast (transform.position, enemyDirection, hit, enemyDirection.magnitude);
+	if (hit.collider && hit.collider.transform == enemyToCheck.transform) {
+		return true;
 	}
+	return false;
 }
